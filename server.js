d4dd8833ab9f17036831f9d4e4a41d8b2eccf997
tetahamer-prog <tetahamer@gmail.com>
2026-03-27 00:52:52 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,17 +4,27 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const path = require('path');
 
+// ፋይሎቹን እንዲያገኝ ማድረግ
+app.use(express.static(__dirname));
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__context, 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// የቻት ግንኙነት
 io.on('connection', (socket) => {
   console.log('A user connected');
+  
   socket.on('chat message', (msg) => {
     io.emit('chat message', msg);
   });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
 });
 
+// ሰርቨሩን ማስጀመር
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
