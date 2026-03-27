f5337e8a8ed9c229eafb0c0d8f9f88a31f40e839
tetahamer-prog <tetahamer@gmail.com>
2026-03-27 06:12:03 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,31 +10,34 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-io.on('connection', (socket) => {
-  console.log('A user connected');
+// ለጊዜው መልዕክቶችን በሜሞሪ ለመያዝ (ዳታቤዝ እስክናገናኝ)
+let msgHistory = [];
 
-  // ሚስጥራዊ ቁልፍ ማረጋገጫ
+io.on('connection', (socket) => {
+  console.log('አዲስ ሰው ተገናኝቷል');
+
+  // የቆዩ መልዕክቶችን ለአዲስ ሰው መላክ
+  socket.emit('chat_history', msgHistory);
+
   socket.on('verify_secure_access', (data) => {
-    // ይህን ቁልፍ ለሰራተኞች ብቻ ነው የምትሰጠው
     const MASTER_KEY = "OMO_SAFETY_2026_SECURE"; 
-    
     if (data.key === MASTER_KEY) {
       socket.emit('login_success', { user: data.username });
     } else {
-      socket.emit('login_failed', { message: "የተሳሳተ የደህንነት ቁልፍ ነው! እባክዎ በትክክል ያስገቡ።" });
+      socket.emit('login_failed', { message: "የተሳሳተ የደህንነት ቁልፍ ነው!" });
     }
   });
 
-  socket.on('chat_message', (msg) => {
-    io.emit('chat_message', msg);
-  });
-
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
+  socket.on('chat_message', (data) => {
+    // መልዕክቱን ታሪክ ውስጥ መመዝገብ
+    msgHistory.push(data);
+    if(msgHistory.length > 100) msgHistory.shift(); // ከ100 በላይ ሲሆን የድሮውን አጥፋ
+    
+    io.emit('chat_message', data);
   });
 });
 
 const PORT = process.env.PORT || 10000;
 http.listen(PORT, () => {
-  console.log(`TETA SECURE is running on port ${PORT}`);
+  console.log(`TETA SECURE is live on port ${PORT}`);
 });
