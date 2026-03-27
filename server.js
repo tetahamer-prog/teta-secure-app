c646346b9daf55fde93150288e68d0200a250631
tetahamer-prog <tetahamer@gmail.com>
2026-03-27 06:32:31 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,21 +4,23 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const path = require('path');
 
+// ፋይሎቹን እንዲያገኝ ማድረግ
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// ለጊዜው መልዕክቶችን በሜሞሪ ለመያዝ (ዳታቤዝ እስክናገናኝ)
+// የመልዕክት ታሪክን ለጊዜው በሜሞሪ ለመያዝ
 let msgHistory = [];
 
 io.on('connection', (socket) => {
-  console.log('አዲስ ሰው ተገናኝቷል');
+  console.log('አዲስ የጸጥታ አባል ተገናኝቷል');
 
-  // የቆዩ መልዕክቶችን ለአዲስ ሰው መላክ
+  // 1. የቆዩ መልዕክቶችን ለአዲስ ገቢ መላክ
   socket.emit('chat_history', msgHistory);
 
+  // 2. የደህንነት ቁልፍ ማረጋገጫ
   socket.on('verify_secure_access', (data) => {
     const MASTER_KEY = "OMO_SAFETY_2026_SECURE"; 
     if (data.key === MASTER_KEY) {
@@ -28,16 +30,34 @@ io.on('connection', (socket) => {
     }
   });
 
+  // 3. መልዕክት ሲላላኩ ታሪክ ውስጥ መመዝገብ
   socket.on('chat_message', (data) => {
-    // መልዕክቱን ታሪክ ውስጥ መመዝገብ
     msgHistory.push(data);
-    if(msgHistory.length > 100) msgHistory.shift(); // ከ100 በላይ ሲሆን የድሮውን አጥፋ
+    // ታሪኩ ከ 200 በላይ እንዳይሆን (ሜሞሪ እንዳይጨናነቅ)
+    if(msgHistory.length > 200) msgHistory.shift(); 
     
     io.emit('chat_message', data);
   });
+
+  // 4. የቪዲዮ/ድምፅ ጥሪ ማስተላለፊያ (Signaling)
+  // አንድ ሰው ሲደውል ለሌላው "ጥሪ እየመጣ ነው" ይላል
+  socket.on('call_user', (data) => {
+    console.log(`${data.from} ጥሪ እየጀመረ ነው...`);
+    socket.broadcast.emit('incoming_call', data);
+  });
+
+  // 5. ጥሪ ሲቋረጥ ለሁሉም ማሳወቅ
+  socket.on('end_call', () => {
+    io.emit('call_ended');
+  });
+
+  socket.on('disconnect', () => {
+    console.log('አባል ተቋርጧል');
+  });
 });
 
+// ሰርቨሩን በ Render ፖርት ወይም በ 10000 ማስጀመር
 const PORT = process.env.PORT || 10000;
 http.listen(PORT, () => {
-  console.log(`TETA SECURE is live on port ${PORT}`);
+  console.log(`🛡️ TETA SECURE Command Center is running on port ${PORT}`);
 });
