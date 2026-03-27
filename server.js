86fa4431ad2a2fb1ef707736a6285a87a7d928d3
tetahamer-prog <tetahamer@gmail.com>
2026-03-27 09:06:05 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,52 +2,49 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, { maxHttpBufferSize: 1e8 });
+const fs = require('fs');
 const path = require('path');
 
 app.use(express.static(__dirname));
 
-let users = {}; // { username: { id: socketId, lastSeen: 'Online', status: 'Available' } }
-let msgHistory = [];
+// ፎልደር የመፍጠር ስራ
+const ensureFolder = (user) => {
+    const dir = path.join(__dirname, 'TETA_RECORDS', user);
+    if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true });
+};
+
+let users = {};
 
 io.on('connection', (socket) => {
     socket.on('verify_secure_access', (data) => {
-        const MASTER_KEY = "OMO_SAFETY_2026_SECURE";
-        if (data.key === MASTER_KEY) {
-            users[data.username] = { id: socket.id, lastSeen: 'Online', status: 'Available' };
+        if (data.key === "OMO_SAFETY_2026_SECURE") {
+            users[data.username] = { id: socket.id, lastSeen: 'Online' };
             socket.username = data.username;
-            socket.emit('login_success', { user: data.username });
+            ensureFolder(data.username); // ፎልደር ክፈት
+            socket.emit('login_success');
             io.emit('user_status', { allUsers: users });
-            socket.emit('chat_history', msgHistory);
-        } else {
-            socket.emit('login_failed', { message: "የተሳሳተ የደህንነት ቁልፍ!" });
         }
     });
 
     socket.on('chat_message', (data) => {
-        if (data.to) {
-            const target = users[data.to];
-            if (target) io.to(target.id).emit('chat_message', data);
-            socket.emit('chat_message', data); 
-        } else {
-            msgHistory.push(data);
-            if(msgHistory.length > 150) msgHistory.shift();
-            io.emit('chat_message', data);
-        }
+        io.emit('chat_message', data);
     });
 
-    socket.on('call_user', (data) => {
-        const target = users[data.to];
-        if (target) io.to(target.id).emit('incoming_call', data);
+    // የተቀረጸ ጥሪን በሰውየው ፎልደር ውስጥ መቅዳት
+    socket.on('save_call_record', (data) => {
+        const filePath = path.join(__dirname, 'TETA_RECORDS', socket.username, data.fileName);
+        const base64Data = data.blob.split(',')[1];
+        fs.writeFile(filePath, base64Data, 'base64', (err) => {
+            if (!err) console.log(`${socket.username} ጥሪ ቀድቶ አስቀምጧል`);
+        });
     });
 
     socket.on('disconnect', () => {
-        if (socket.username && users[socket.username]) {
-            users[socket.username].lastSeen = new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+        if (socket.username) {
+            users[socket.username].lastSeen = new Date().toLocaleTimeString();
             io.emit('user_status', { allUsers: users });
-            delete users[socket.username].id; // ግንኙነቱ መቋረጡን ለማወቅ
         }
     });
 });
 
-const PORT = process.env.PORT || 10000;
-http.listen(PORT, () => console.log(`TETA SECURE PRO Live on ${PORT}`));
+http.listen(10000, () => console.log('TETA SECURE v3.0 FULL PRO - Running...'));
